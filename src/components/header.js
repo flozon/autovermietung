4d125003ababaac85e16205f501ee5f76594a0c4
flozon <3.14159265358979@web.de>
2020-03-17 18:26:10 +0100

    Erster Test

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+	return (
+		<div className="Header">
+			<div class="row blue" id="contact">
+				<div class="col s11">This div is 12-columns wide on all screen sizes</div>
+			</div>
+		</div>
+	)
+}
+export default Header
