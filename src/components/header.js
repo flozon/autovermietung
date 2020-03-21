a08b01f0a6345a6505d64660bc318979ffc12430
flozon <3.14159265358979@web.de>
2020-03-21 14:10:24 +0100

    erster Test

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,8 @@ function Header() {
 	return (
 		<div className="Header">
 			<div class="row blue" id="contact">
-				<div class="col s11">This div is 12-columns wide on all screen sizes</div>
+				<div class="col s10">This div is 12-columns wide on all screen sizes</div>
+				<div class="col s2">Filter</div>
 			</div>
 		</div>
 	)
